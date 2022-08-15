2403dc7f64fc1ab97c51b0d5c0923911992a9d6a
Dilawar Khan Niazi <dilawar.niazi.dn@gmail.com>
2022-08-15 16:34:18 +0500

    made the planets html

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const {Email, emailValidate} = require('../models/email');
 
 app.get('/', (req, res) => {
     return res.redirect('home.html')
-});
-
-app.post('/submitted', async(req, res) => {
-    const { error } = emailValidate(req.body);
-    if(error) return res.redirect('./write_invalid.html');
-
-    let email = new Email({
-        email: req.body.email,
-        text: req.body.text
-    })
-
-    email = await email.save();
-    
-    res.redirect('./submitted.html');
 });
 
 module.exports = app;
